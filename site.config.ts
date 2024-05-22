c71a4ec168bf996f61cc54ff612939a40f143ec1
Felix Nguyen <im.felixng@gmail.com>
2024-05-23 01:17:39 +0700

    feat: update notionID

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from "./lib/site-config";
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: "4e4fb181f4d345bbb27676be974e58b7",
+  rootNotionPageId: "57a665e69c424c9a868d7382b3a6d654",
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
